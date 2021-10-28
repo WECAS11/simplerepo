6620374a1e0ccfced811ce611eb5d4253807a6d2
vikas hiremath <vikashiremath55@gmail.com>
2021-10-28 10:02:29 +0530

    added prime file

--- add.js
+++ add.js
@@ -1,1 +1,6 @@
-console.log(fuc1)
+const firstNumber = 5;
+const secondNumber = 6;
+var result = firstNumber+secondNumber;
+console.log("firstnum", firstNumber);
+console.log("secondnum", secondNumber);
+console.log( "result", result);
--- sub.js
+++ sub.js
@@ -1,1 +1,9 @@
-console.log("sss")
+var a = 7;
+var b = 6;
+var c = a % b;
+if(c/2){
+    console.log('its not divisible');
+
+}else{
+    console.log('divisible by 2');
+}
