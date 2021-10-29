9782e3e36b377400ecbaa7589139272fb4317de1
vikas hiremath <vikashiremath55@gmail.com>
2021-10-29 10:42:12 +0530

    prime number

--- prime.js
+++ prime.js
@@ -1,4 +1,15 @@
-var a=10;
-if (a%a === 0){
-
+var num = 8;
+if(num == 1){
+    console.log( num , "is neither a prime nor composite.");
+}else if(num < 1){
+     console.log(num , "is not a prime number");
+}else{
+    for(var i = 2; i<num; i++){
+        if(num%i === 0){
+          console.log(num, "is not a prime number");
+          break;
+        }else{
+            console.log(num, "is prime number");
+        }
+    }
 }
